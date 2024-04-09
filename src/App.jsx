ec72e0bfc0ed250405a3582f1bfc9fea9a8f992e
owais3 <khan6awais@gmail.com>
2024-04-09 22:01:19 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,272 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1>Ramadan Kareem</h1>
+        <p className='title'>Sehar and iftar time schedule || 2024</p>
+        
+        <table>
+          <thead>
+            <tr id='header'> 
+              <th className='roza'>Roza</th>
+              <th className='date'>Date</th>
+              <th className='day'>Day</th>
+              <th className='sehr'>Sehr</th>
+              <th className='iftar'>Iftar</th>
+            </tr>
+          </thead>
+        <tbody>
+
+          <tr>
+            <td>01</td>
+            <td>12 March</td>
+            <td>Tue</td>
+            <td>05:27</td>
+            <td>06:41</td>
+          </tr>
+
+          <tr>
+            <td>02</td>
+            <td>13 March</td>
+            <td>Wed</td>
+            <td>05:26</td>
+            <td>06:41</td>
+          </tr>
+
+          <tr>
+            <td>03</td>
+            <td>14 March</td>
+            <td>Thu</td>
+            <td>05:25</td>
+            <td>06:41</td>
+          </tr>
+
+          <tr>
+            <td>04</td>
+            <td>15 March</td>
+            <td>Fri</td>
+            <td>05:24</td>
+            <td>06:42</td>
+          </tr>
+
+          <tr>
+            <td>05</td>
+            <td>16 March</td>
+            <td>Sat</td>
+            <td>05:23</td>
+            <td>06:42</td>
+          </tr>
+
+          <tr>
+            <td>06</td>
+            <td>17 March</td>
+            <td>Sun</td>
+            <td>05:22</td>
+            <td>06:43</td>
+          </tr>
+
+          <tr>
+            <td>07</td>
+            <td>18 March</td>
+            <td>Mon</td>
+            <td>05:21</td>
+            <td>06:43</td>
+          </tr>
+
+          <tr>
+            <td>08</td>
+            <td>19 March</td>
+            <td>Tue</td>
+            <td>05:20</td>
+            <td>06:44</td>
+          </tr>
+
+          <tr>
+            <td>09</td>
+            <td>20 March</td>
+            <td>Wed</td>
+            <td>05:19</td>
+            <td>06:44</td>
+          </tr>
+
+          <tr>
+            <td>10</td>
+            <td>21 March</td>
+            <td>Thu</td>
+            <td>05:18</td>
+            <td>06:45</td>
+          </tr>
+
+          <tr>
+            <td>11</td>
+            <td>22 March</td>
+            <td>Fri</td>
+            <td>05:17</td>
+            <td>06:45</td>
+          </tr>
+
+          <tr>
+            <td>12</td>
+            <td>23 March</td>
+            <td>Sat</td>
+            <td>05:15</td>
+            <td>06:45</td>
+          </tr>
+
+          <tr>
+            <td>13</td>
+            <td>24 March</td>
+            <td>Sun</td>
+            <td>05:14</td>
+            <td>06:46</td>
+          </tr>
+
+          <tr>
+            <td>14</td>
+            <td>25 March</td>
+            <td>Mon</td>
+            <td>05:13</td>
+            <td>06:46</td>
+          </tr>
+
+          <tr>
+            <td>15</td>
+            <td>26 March</td>
+            <td>Tue</td>
+            <td>05:12</td>
+            <td>06:47</td>
+          </tr>
+
+          <tr>
+            <td>16</td>
+            <td>27 March</td>
+            <td>Wed</td>
+            <td>05:11</td>
+            <td>06:47</td>
+          </tr>
+
+          <tr>
+            <td>17</td>
+            <td>28 March</td>
+            <td>Thu</td>
+            <td>05:10</td>
+            <td>06:48</td>
+          </tr>
+
+          <tr>
+            <td>18</td>
+            <td>29 March</td>
+            <td>Fri</td>
+            <td>05:09</td>
+            <td>06:48</td>
+          </tr>
+
+          <tr>
+            <td>19</td>
+            <td>30 March</td>
+            <td>Sat</td>
+            <td>05:08</td>
+            <td>06:48</td>
+          </tr>
+
+          <tr>
+            <td>20</td>
+            <td>31 March</td>
+            <td>Sun</td>
+            <td>05:07</td>
+            <td>06:49</td>
+          </tr>
+
+          <tr>
+            <td>21</td>
+            <td>01 April</td>
+            <td>Mon</td>
+            <td>05:05</td>
+            <td>06:49</td>
+          </tr>
+
+          <tr>
+            <td>22</td>
+            <td>02 April</td>
+            <td>Tue</td>
+            <td>05:04</td>
+            <td>06:50</td>
+          </tr>
+
+          <tr>
+            <td>23</td>
+            <td>03 April</td>
+            <td>Wed</td>
+            <td>05:03</td>
+            <td>06:50</td>
+          </tr>
+
+          <tr>
+            <td>24</td>
+            <td>04 April</td>
+            <td>Thu</td>
+            <td>05:02</td>
+            <td>06:51</td>
+          </tr>
+
+          <tr>
+            <td>25</td>
+            <td>05 April</td>
+            <td>Fri</td>
+            <td>05:01</td>
+            <td>06:51</td>
+          </tr>
+
+          <tr>
+            <td>26</td>
+            <td>06 April</td>
+            <td>Sat</td>
+            <td>05:00</td>
+            <td>06:51</td>
+          </tr>
+
+          <tr>
+            <td>27</td>
+            <td>07 April</td>
+            <td>Sun</td>
+            <td>04:59</td>
+            <td>06:52</td>
+          </tr>
+
+          <tr>
+            <td>28</td>
+            <td>08 April</td>
+            <td>Mon</td>
+            <td>04:58</td>
+            <td>06:52</td>
+          </tr>
+
+          <tr>
+            <td>29</td>
+            <td>09 April</td>
+            <td>Tue</td>
+            <td>04:57</td>
+            <td>06:53</td>
+          </tr>
+
+          <tr>
+            <td>30</td>
+            <td>10 April</td>
+            <td>Wed</td>
+            <td>04:55</td>
+            <td>06:53</td>
+          </tr>
+
+        </tbody>
+        </table>
+
+      </div>
+    </>
+  )
+}
+
+export default App
